0a634d612b02bbb5e9e5dee7783e74d2215beedc
Arjuna Anuhass <anuhass2017@gmail.com>
2024-04-29 10:03:56 +0530

    Bug Fixed - CreateRestaurantRequest Class
    this request class didn't have a address field so i fixed it.

--- src/main/java/com/aa_code/online_food_ordering_backend/request/CreateRestaurantRequest.java
+++ src/main/java/com/aa_code/online_food_ordering_backend/request/CreateRestaurantRequest.java
@@ -1,5 +1,6 @@
 package com.aa_code.online_food_ordering_backend.request;
 
+import com.aa_code.online_food_ordering_backend.Model.Address;
 import com.aa_code.online_food_ordering_backend.Model.ContactInformation;
 import lombok.Data;
 
@@ -13,6 +14,7 @@ public class CreateRestaurantRequest {
     private String name;
     private String description;
     private String cuisineType;
+    private Address address;
     private ContactInformation contactInformation;
     private String openingHours;
     private List<String> images;
